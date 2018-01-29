558fa4a07674bb90d505d735a370add385303f73
VamsiGandra <sizrvamsi@gmail.com>
2018-01-29 13:18:01 +1300

    Added redux boiler plate

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,14 @@
+
+const INITIAL_STATE = {
+    loading: ''
+}
+
+export default (state = INITIAL_STATE, action) => { 
+
+    switch(action.type) {
+
+        default:
+            return state;
+    }
+
+};
